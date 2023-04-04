90bac1150b69220be17c30f638e0fc0c6ab6b2ae
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-04 12:47:52 +0300

    connect to MongoDB

--- server/model/Favourites.js
+++ server/model/Favourites.js
@@ -3,9 +3,22 @@ const { Schema, model } = mongoose;
 
 const favouritesSchema = new Schema({
   imdbID: String,
-  your_rating: String,
+  your_rating: Number,
 });
 
 const Favourites = model("Favourites", favouritesSchema);
+
+const createFavourites = async () => {
+  try {
+    const favourite = await Favourites.create({
+      imdbID: "tt0372784",
+      your_rating: 7,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+createFavourites();
 
 module.exports = Favourites;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Favourites = require("./model/favourites.js");
 
 const app = express();
 const PORT = 3001;
@@ -9,7 +10,7 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -19,20 +20,32 @@ app.use(function (req, res, next) {
 
 //Connect to MongoDB
 (async () => {
-    try {
-      await mongoose.connect(
-        "mongodb://localhost:27017/local/favourites",
-        { useNewUrlParser: true, useUnifiedTopology: true }
-      );
-      console.log("Successfully connected to MongoDB database!");
-    } catch (err) {
-      console.error("Error connecting to MongoDB database:", err);
-    }
-  })();
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/MERN_project");
+    console.log("Successfully connected to MongoDB database!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB database:", err);
+  }
+})();
 
+app.get("/", async (req, res) => {
+  const imdbID = "asfasdasfas";
+  const your_rating = 9;
 
-  
+  const favourites = new Favourites({
+    imdbID,
+    your_rating,
+  });
+
+  try {
+    const saved = await favourites.save();
+    res.json(saved);
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+});
+
 //Start server
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-  });
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
