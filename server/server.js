eecdd5b4d2d5d221462447396bd5597bc1d63c9f
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-04 11:46:44 +0300

    middleware & connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -5,6 +5,30 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3001;
 
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+//Connect to MongoDB
+(async () => {
+    try {
+      await mongoose.connect(
+        "",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      );
+      console.log("Successfully connected to MongoDB database!");
+    } catch (err) {
+      console.error("Error connecting to MongoDB database:", err);
+    }
+  })();
 
 //Start server
 app.listen(PORT, () => {
