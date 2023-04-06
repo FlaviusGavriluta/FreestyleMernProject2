c20c1a688bcd6149b6d79ba1ec03c9171f7aea5f
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-06 17:46:44 +0300

    fixed search button rendering

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { Navbar } from "./Navbar/Navbar";
 import { MoviesSearch } from "./MoviesSearch";
+import { DisplayMovie } from "./DisplayMovie";
 
 const API_KEY = "75dd50d6";
 
 export const Movies = () => {
   const [title, setTitle] = useState("");
   const [movieSearch, setMovieSearch] = useState([]);
-  const [isPressed, setIsPressed] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   const handleSearch = async () => {
     try {
@@ -19,15 +21,20 @@ export const Movies = () => {
       }
       const movies = await response.json();
       if (movies.Search) setMovieSearch(movies.Search);
+      setIsSearching(true);
+      setSelectedMovie(null);
     } catch (err) {
       console.log(err);
     }
+  };
+  const handleMovieClick = (movieId) => {
+    setSelectedMovie(movieSearch.find((movie) => movie.imdbID === movieId));
+    setIsSearching(false);
   };
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       handleSearch();
-      setIsPressed(true);
     }
   };
 
@@ -38,11 +45,15 @@ export const Movies = () => {
         search={handleSearch}
         handleKeyPress={handleKeyPress}
       />
-      {isPressed ? (
-        <MoviesSearch movies={movieSearch} title={title} />
-      ) : (
-        <div>No movies found</div>
+      {isSearching && !selectedMovie && (
+        <MoviesSearch
+          movies={movieSearch}
+          title={title}
+          onClick={handleMovieClick}
+        />
       )}
+      {selectedMovie && <DisplayMovie imdbID={selectedMovie.imdbID} />}
+      {!isSearching && !selectedMovie && <div>No movies found</div>}
     </div>
   );
 };
--- client/src/components/MoviesSearch.js
+++ client/src/components/MoviesSearch.js
@@ -1,48 +1,31 @@
-import { useState } from "react";
-import { DisplayMovie } from "./DisplayMovie";
-
-export const MoviesSearch = ({ movies, title }) => {
-  const [id, setId] = useState("");
-  const [isClicked, setIsClicked] = useState(false);
-
-  return (
-    <div>
-      {isClicked ? (
-        <DisplayMovie imdbID={id}/>
-      ) : (
-        <div className="container border-light">
-          <div className="row justify-content-md-start">
-            <h1 className="my-3">Search "{title}"</h1>
-            <h3 className="my-3">
-              <i class="bi bi-grip-vertical"></i>Titles
-            </h3>
-            {movies.map((movie, i) => (
-              <div key={i}>
-                <div
-                  className="card border-light mb-3"
-                  style={{ maxWidth: "auto", maxHeight: "75px" }}
-                >
-                  <div className="row" style={{ alignItems: "center" }}>
-                    <div
-                      className="btn col-md-1"
-                      onClick={() => {
-                        setIsClicked(true);
-                        setId(movie.imdbID);
-                      }}
-                    >
-                      <img src={movie.Poster} style={{ maxWidth: "50px" }} />
-                    </div>
-                    <div className="col">
-                      <div className="fw-bold">{movie.Title}</div>
-                      <div className="text-secondary">{movie.Year}</div>
-                    </div>
-                  </div>
-                </div>
+export const MoviesSearch = ({ movies, title, onClick }) => (
+  <div className="container border-light">
+    <div className="row justify-content-md-start">
+      <h1 className="my-3">Search "{title}"</h1>
+      <h3 className="my-3">
+        <i class="bi bi-grip-vertical"></i>Titles
+      </h3>
+      {movies.map((movie) => (
+        <div key={movie.imdbID}>
+          <div
+            className="card border-light mb-3"
+            style={{ maxWidth: "auto", maxHeight: "75px" }}
+          >
+            <div className="row" style={{ alignItems: "center" }}>
+              <div
+                className="btn col-md-1"
+                onClick={() => onClick(movie.imdbID)}
+              >
+                <img src={movie.Poster} style={{ maxWidth: "50px" }} />
               </div>
-            ))}
+              <div className="col">
+                <div className="fw-bold">{movie.Title}</div>
+                <div className="text-secondary">{movie.Year}</div>
+              </div>
+            </div>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  );
-};
+  </div>
+);
