38e993421311f49fa5c8e2c278bd12e16319d8d6
ChircuAndrei <chircu.andrei93@gmail.com>
2024-02-07 21:50:25 +0200

    added dropdown

--- client/src/components/Navbar/components/Watchlist.js
+++ client/src/components/Navbar/components/Watchlist.js
@@ -13,7 +13,7 @@ export const Watchlist = (favorites) => (
        <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="#">Action</a></li>
                 <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+
             </ul>
         </div>
 
